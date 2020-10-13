8140120478bbcd8f19fc3b5ed2a2d02e116d37f7
Zeeshan Haider Shaheen <p156064@nu.edu.pk>
2020-10-13 17:57:17 +0500

    Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    }
+  },
+  variants: {},
+  plugins: []
+}
